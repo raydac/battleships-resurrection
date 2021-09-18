fdb55e9bee0744684317a274e3a26ff60ffbaee0
Igor Maznitsa <rrg4400@gmail.com>
2021-09-18 12:16:54 +0300

    removed log save from default linux script

--- battleships-resurrection/battleships-client-pc/src/assembly/linux/run.sh
+++ battleships-resurrection/battleships-client-pc/src/assembly/linux/run.sh
@@ -16,7 +16,6 @@
 #
 
 BATTLESHIPS_HOME="$(dirname ${BASH_SOURCE[0]})"
-LOG_FILE=$BATTLESHIPS_HOME/console.log
 JAVA_HOME=$BATTLESHIPS_HOME/jre
 
 #JAVA_EXTRA_GFX_FLAGS="-Dcom.sun.management.jmxremote=true -Dsun.java2d.opengl=true"
@@ -26,14 +25,6 @@ JAVA_FLAGS="-server -Dsun.rmi.transport.tcp.maxConnectionThreads=0 -XX:+DisableA
 
 JAVA_RUN=$JAVA_HOME/bin/java
 
-echo \$JAVA_RUN=$JAVA_RUN &>$LOG_FILE
-
-echo ------JAVA_VERSION------ &>>$LOG_FILE
-
-$JAVA_RUN -version &>>$LOG_FILE
-
-echo ------------------------ &>>$LOG_FILE
-
-$JAVA_RUN $JAVA_FLAGS $JAVA_EXTRA_GFX_FLAGS -Djava.library.path="$BATTLESHIPS_HOME" -jar "$BATTLESHIPS_HOME"/battleships-resurrection.jar $@ &>>$LOG_FILE&
+$JAVA_RUN $JAVA_FLAGS $JAVA_EXTRA_GFX_FLAGS -Djava.library.path="$BATTLESHIPS_HOME" -jar "$BATTLESHIPS_HOME"/battleships-resurrection.jar $@
 
 exit 0
