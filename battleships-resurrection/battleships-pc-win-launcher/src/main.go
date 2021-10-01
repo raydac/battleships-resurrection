9104ba035320b8649e8ac19011e6f12d6a767115
Igor Maznitsa <rrg4400@gmail.com>
2021-10-02 00:24:38 +0300

    refactoring

--- battleships-resurrection/battleships-pc-win-launcher/src/main.go
+++ battleships-resurrection/battleships-pc-win-launcher/src/main.go
@@ -8,17 +8,22 @@ import (
 	"path/filepath"
 )
 
-//go:generate goversioninfo
+//go:generate goversioninfo -gofile=versioninfo.go
+
+const JDK_PATH = "\\jre\\bin\\javaw.exe"
+const JAR_FILE = "\\battleships-resurrection.jar"
 
 func main() {
-	fmt.Println("Battleships PC client launcher")
+	fmt.Printf("%v %v\n%v\n", versionInfo.StringFileInfo.ProductName, versionInfo.StringFileInfo.ProductVersion, versionInfo.StringFileInfo.LegalCopyright)
 	path, err := os.Executable()
 	if err == nil {
 		base_folder := filepath.Dir(path)
-		cmd := exec.Command(base_folder+"\\jre\\bin\\javaw.exe", "-Dsun.java2d.opengl=true", "-jar", base_folder+"\\battleships-resurrection.jar")
+		cmd := exec.Command(base_folder+JDK_PATH, "-Dsun.java2d.opengl=true", "-jar", base_folder+JAR_FILE)
 		err = cmd.Start()
 		if err != nil {
 			log.Fatal(err)
 		}
+	} else {
+		log.Fatal(err)
 	}
 }
